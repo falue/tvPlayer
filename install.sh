84c0f022d9720b4f2d5ff026f6675b10845da3dd
falue <info@fluescher.ch>
2024-10-03 13:54:34 +0200

    change toUSER_HOME for UBS-pop-up settings; remove debug prints

--- install.sh
+++ install.sh
@@ -14,32 +14,20 @@ pip install -r requirements.txt
 
 # Get the full path of the script to be executed
 USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
-echo "USER_HOME:"
-echo "$USER_HOME"
-
 SCRIPT_PATH="$(realpath $0)"
 SCRIPT_DIR="$(dirname $SCRIPT_PATH)"
 SCRIPT_NAME="tvPlayer.py"
 FULL_SCRIPT_PATH="$SCRIPT_DIR/$SCRIPT_NAME"
-
-echo "FULL_SCRIPT_PATH:"
-echo "$FULL_SCRIPT_PATH"
 
 # Make the Python script executable
 chmod +x "$FULL_SCRIPT_PATH"
 
 # Create the autostart directory if it doesn't exist
 AUTOSTART_DIR="$USER_HOME/.config/autostart"
-echo "AUTOSTART_DIR:"
-echo "$AUTOSTART_DIR"
-
 mkdir -p "$AUTOSTART_DIR"
 
 # Create the .desktop file for autostart
 DESKTOP_FILE="$AUTOSTART_DIR/tvPlayer.desktop"
-
-echo "DESKTOP_FILE:"
-echo "$DESKTOP_FILE"
 
 echo "Creating autostart file..."
 cat > "$DESKTOP_FILE" << EOL
@@ -53,13 +41,13 @@ EOL
 echo "Autostart file created at $DESKTOP_FILE"
 
 # Disable the "removable medium is inserted" pop-up in PCManFM
-PCMANFM_CONF="$HOME/.config/pcmanfm/LXDE/pcmanfm.conf"
+PCMANFM_CONF="$USER_HOME/.config/pcmanfm/LXDE/pcmanfm.conf"
 if [ -f "$PCMANFM_CONF" ]; then
     # Modify the existing config file
     sed -i '/\[volume\]/,/\[/{s/mount_open=1/mount_open=0/}' "$PCMANFM_CONF"
 else
     # Create the config file if it doesn't exist
-    mkdir -p "$HOME/.config/pcmanfm/LXDE"
+    mkdir -p "$USER_HOME/.config/pcmanfm/LXDE"
     cat > "$PCMANFM_CONF" << EOL
 [volume]
 mount_open=0
