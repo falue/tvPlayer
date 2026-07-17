864ee421ad6e8b7a4c4854b59c277b9a01d50b32
falue <info@fluescher.ch>
2026-07-17 14:44:45 +0200

    When crashing, readd MQTT anyways

--- tvPlayer.py
+++ tvPlayer.py
@@ -85,6 +85,7 @@ def udp_init(port=53534):
     """
     def udp_listener():
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sock.bind(("0.0.0.0", port))
         try:
             local_ip = subprocess.check_output(['hostname', '-I']).decode().strip().split()[0]
