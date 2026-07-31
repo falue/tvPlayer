5b24d474c1682d802baf192c5b16a717dc9644ed
falue <info@fluescher.ch>
2026-07-31 15:14:25 +0200

    Bugfix: Default imag display duration of MPV not 5s but inf

--- tvPlayer.py
+++ tvPlayer.py
@@ -450,6 +450,7 @@ def player_init():
     player = mpv.MPV(
         vo='drm',
         loop_file='inf',
+        image_display_duration='inf',
         idle=True,
         # Disable all mpv UI elements
         osc=False,
