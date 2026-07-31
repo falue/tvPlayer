40073a95b6b028881c82a2758d9f39d5a9c4186b
falue <info@fluescher.ch>
2026-07-31 15:29:45 +0200

    Test

--- tvPlayer.py
+++ tvPlayer.py
@@ -450,7 +450,7 @@ def player_init():
     player = mpv.MPV(
         vo='drm',
         loop_file='inf',
-        image_display_duration='inf',
+        image_display_duration=1,
         idle=True,
         # Disable all mpv UI elements
         osc=False,
