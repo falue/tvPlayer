1536d47799111096190c97c180fd134ff6403d02
falue <info@fluescher.ch>
2026-07-31 16:04:37 +0200

    Remove unneeded classes

--- webremote/scripts.js
+++ webremote/scripts.js
@@ -317,10 +317,8 @@ function handleState(data, fillColor=false) {
     tvChannel = data.tvChannel;
     if (data.isPlaying) {
       gebi("playstate").src = "./assets/icons/pause.svg";
-      gebi("playstate").className = "pause";
     } else {
       gebi("playstate").src = "./assets/icons/play.svg";
-      gebi("playstate").className = "play";
     }
 
     if (data.currentFileName.length > 0 && !fillColor) {
